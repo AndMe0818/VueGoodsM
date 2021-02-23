1573d653403221883e69b91d7de22a367f3b00b5
leizi <1102928566@qq.com>
2021-02-23 10:39:04 +0800

    第二次测试

---
+++ src/views/Pome.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>很大声</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Pome',
+   
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style  scoped>
+
+</style>
